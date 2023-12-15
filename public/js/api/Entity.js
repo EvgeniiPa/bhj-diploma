f0cb645d5864754c7de0f4b558f1d8e5cf34b014
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2023-12-15 15:48:56 +0300

    Diploma 15.12

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -8,24 +8,18 @@ class Entity {
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static list(data, callback){
-
-  }
+  static list(data, callback) {}
 
   /**
    * Создаёт счёт или доход/расход с помощью запроса
    * на сервер. (в зависимости от того,
    * что наследуется от Entity)
    * */
-  static create(data, callback) {
-
-  }
+  static create(data, callback) {}
 
   /**
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static remove(data, callback ) {
-
-  }
+  static remove(data, callback) {}
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,36 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  let xhr = new XMLHttpRequest();
+  let method = options.method;
 
+  let url =
+    options.url +
+    "?mail=" +
+    options.data.mail +
+    "&password=" +
+    options.data.password;
+
+  if (method == "GET") {
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.send();
+  } else if (method != "GET") {
+    let formData = new FormData();
+    formData.append("mail", options.data.mail);
+    formData.append("password", options.data.password);
+
+    xhr.open("POST", url);
+    xhr.responseType = "json";
+    xhr.send(formData);
+  }
+
+  
+  xhr.onload = () => {
+    options.callback();
+  };
+
+  xhr.onerror = () => {
+    options.callback(xhr.response);
+  };
 };
